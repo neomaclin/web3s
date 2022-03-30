9c00606b20d83c09faeeb12f1048e08d329a4bfc
Neo Lin <neomac.lin@gmail.com>
2022-03-30 01:06:22 -0400

    BipTest pass

--- modules/crypto/src/main/scala/org/web3s/crypto/Bip32ECKeyPair.scala
+++ modules/crypto/src/main/scala/org/web3s/crypto/Bip32ECKeyPair.scala
@@ -11,21 +11,21 @@ object Bip32ECKeyPair:
   val HARDENED_BIT = 0x80000000
 
   def create(privateKey: BigInt, chainCode: Array[Byte]): Bip32ECKeyPair =
-     Bip32ECKeyPair(Some(privateKey), Sign.publicKeyFromPrivate(privateKey), 0, chainCode, None)
+    Bip32ECKeyPair(Some(privateKey), Sign.publicKeyFromPrivate(privateKey), 0, chainCode, None)
 
-  def create(privateKey: Array[Byte], chainCode: Array[Byte]):Bip32ECKeyPair =
+  def create(privateKey: Array[Byte], chainCode: Array[Byte]): Bip32ECKeyPair =
     create(Numeric.toBigInt(privateKey), chainCode)
 
   def generateKeyPair(seed: Array[Byte]): Bip32ECKeyPair =
-    val (il,ir) =  hmacSha512("Bitcoin seed".getBytes, seed).splitAt(32)
+    val (il, ir) = hmacSha512("Bitcoin seed".getBytes, seed).splitAt(32)
     val keypair = Bip32ECKeyPair.create(il, ir.take(32))
     keypair
   end generateKeyPair
 
-  def deriveKeyPair(master: Bip32ECKeyPair, path: Array[Int]):Bip32ECKeyPair =
+  def deriveKeyPair(master: Bip32ECKeyPair, path: Array[Int]): Bip32ECKeyPair =
     path.foldLeft(master)(_.deriveChildKey(_))
 
-  private def bigIntegerToBytes32(b: BigInt):Array[Byte] =
+  private def bigIntegerToBytes32(b: BigInt): Array[Byte] =
     val numBytes = 32
     val src = b.toByteArray
     val dest = new Array[Byte](numBytes)
@@ -44,54 +44,52 @@ end Bip32ECKeyPair
 
 final case class Bip32ECKeyPair(privateKey: Option[BigInt],
                                 publicKey: BigInt,
-                                childNumber: Int, 
+                                childNumber: Int,
                                 chainCode: Array[Byte],
-                                parent: Option[Bip32ECKeyPair])  {
-  def parentHasPrivate: Boolean = parent.nonEmpty && parent.exists(_.hasPrivateKey)
- // this.parentFingerprint = if (parent != null) parent.getFingerprint else 0
-//  final private var parentHasPrivate = false
-//  final private var depth = 0
-//  final private var chainCode = null
-//  private var parentFingerprint = 0
-//  private var publicKeyPoint = null
+                                parent: Option[Bip32ECKeyPair]) {
+  private def parentHasPrivate: Boolean = parent.nonEmpty && parent.exists(_.hasPrivateKey)
+  // this.parentFingerprint = if (parent != null) parent.getFingerprint else 0
+  //  final private var parentHasPrivate = false
+  //  final private var depth = 0
+  //  final private var chainCode = null
+  //  private var parentFingerprint = 0
+  //  private var publicKeyPoint = null
 
-  private def deriveChildKey(childNumber: Int): Bip32ECKeyPair = ???
-//    if (!hasPrivateKey) {
-//    val parentPublicKey = publicKeyPoint.getEncoded(true)
-//    val data = ByteBuffer.allocate(37)
-//    data.put(parentPublicKey)
-//    data.putInt(childNumber)
-//    val i = hmacSha512(getChainCode, data.array)
-//    val il = util.Arrays.copyOfRange(i, 0, 32)
-//    val chainCode = util.Arrays.copyOfRange(i, 32, 64)
-//    Array.fill(i, 0.toByte)
-//    val ilInt = BigInt(1, il)
-//    Array.fill(il, 0.toByte)
-//    val ki = Sign.publicPointFromPrivate(ilInt).add(getPublicKeyPoint)
-//    Bip32ECKeyPair(null, Sign.publicFromPoint(ki.getEncoded(true)), childNumber, chainCode, Some(this))
-//  } else {
-//    val data = ByteBuffer.allocate(37)
-//    if (Bip32ECKeyPair.isHardened(childNumber)) data.put(getPrivateKeyBytes33)
-//    else {
-//      val parentPublicKey = publicKeyPoint.getEncoded(true)
-//      data.put(parentPublicKey)
-//    }
-//    data.putInt(childNumber)
-//    val i = hmacSha512(chainCode, data.array)
-//    val il = util.Arrays.copyOfRange(i, 0, 32)
-//    val chainCode = util.Arrays.copyOfRange(i, 32, 64)
-//    util.Arrays.fill(i, 0.toByte)
-//    val ilInt = BigInt(1, il)
-//    util.Arrays.fill(il, 0.toByte)
-//    val privateKey1 = privateKey + ilInt % Sign.CURVE.getN
-//    new Bip32ECKeyPair(privateKey1, Sign.publicKeyFromPrivate(privateKey1), childNumber, chainCode, this)
-//  }
+  private def deriveChildKey(childNumber: Int): Bip32ECKeyPair =
+    if (!hasPrivateKey) {
+      val parentPublicKey = publicKeyPoint.getEncoded(true)
+      val data = ByteBuffer.allocate(37)
+      data.put(parentPublicKey)
+      data.putInt(childNumber)
+      val i = hmacSha512(chainCode, data.array)
+      val il = i.slice(0, 32)
+      val chainCodeLocal = i.slice(32, 64)
+      val ilInt = BigInt(1, il)
+      val ki = Sign.publicPointFromPrivate(ilInt).add(publicKeyPoint)
+      Bip32ECKeyPair(None, Sign.publicFromPoint(ki.getEncoded(true)), childNumber, chainCodeLocal, Some(this))
+    } else {
+      val data = ByteBuffer.allocate(37)
+      if (Bip32ECKeyPair.isHardened(childNumber))
+        data.put(privateKeyBytes33)
+      else
+        data.put(publicKeyPoint.getEncoded(true))
+      end if
+      data.putInt(childNumber)
+      val i = hmacSha512(chainCode, data.array)
+      val il = i.slice(0, 32)
+      val chainCodeLocal = i.slice(32, 64)
+      val ilInt = BigInt(1, il)
+      val privateKey1 = privateKey.map(i => (i + ilInt) % Sign.CURVE.getN)
+      Bip32ECKeyPair(privateKey1, Sign.publicKeyFromPrivate(privateKey1.getOrElse(BigInt(0))), childNumber, chainCodeLocal, Some(this))
+    }
+  end deriveChildKey
+
 
   private def fingerprint: Int =
     val id = identifier
     id(3) & 0xFF | (id(2) & 0xFF) << 8 | (id(1) & 0xFF) << 16 | (id(0) & 0xFF) << 24
   end fingerprint
-   
+
 
   def depth: Int = parent.fold(0)(_.depth + 1)
 
@@ -99,16 +97,16 @@ final case class Bip32ECKeyPair(privateKey: Option[BigInt],
 
   private def identifier = sha256hash160(publicKeyPoint.getEncoded(true))
 
-  def publicKeyPoint: ECPoint = ??? //Sign.publicPointFromPrivate(getPrivateKey)
-//
-//
-  def privateKeyBytes33: Array[Byte] = ???
-//    val numBytes = 33
-//    val bytes33 = new Array[Byte](numBytes)
-//    val priv = Bip32ECKeyPair.bigIntegerToBytes32(privateKey)
-//    Array.copy(priv, 0, bytes33, numBytes - priv.length, priv.length)
-//    bytes33
-//  end privateKeyBytes33
+  def publicKeyPoint: ECPoint = Sign.publicPointFromPrivate(privateKey.getOrElse(BigInt(0)))
+
+  def privateKeyBytes33: Array[Byte] =
+    val numBytes = 33
+    val bytes33 = new Array[Byte](numBytes)
+    val priv = Bip32ECKeyPair.bigIntegerToBytes32(privateKey.getOrElse(BigInt(0)))
+    Array.copy(priv, 0, bytes33, numBytes - priv.length, priv.length)
+    bytes33
+  end privateKeyBytes33
 
   private def hasPrivateKey = privateKey.nonEmpty || parentHasPrivate
+
 }
--- modules/crypto/src/test/scala/org/web3s/crypto/Base58.scala
+++ modules/crypto/src/test/scala/org/web3s/crypto/Base58.scala
@@ -1,10 +1,9 @@
-
 package org.web3s.crypto
 
 import scala.collection.mutable
 
-
 object Base58:
+
   private val ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".toCharArray
   private val ENCODED_ZERO = ALPHABET(0)
 
@@ -12,42 +11,47 @@ object Base58:
     if (input.length == 0)
       ""
     else
-      val zeros = input.takeWhile(_ == 0).sum
+      var zeros = input.takeWhile(_ == 0).sum
+
       // Convert base-256 digits to base-58 digits (plus conversion to ASCII characters)
-      val inputClone = Array.from(input) // since we modify it in-place
+      var inputClone = Array.from(input) // since we modify it in-place
       val encoded = new Array[Char](input.length * 2) // upper bound
       var outputStart = encoded.length
       var inputStart = zeros
-      //      while ( {
-      //        inputStart < input.length
-      //
-      //        encoded({
-      //          outputStart -= 1; outputStart
-      //        }) = ALPHABET(divmod(input, inputStart, 256, 58))
-      //        if (input(inputStart) == 0) inputStart += 1 // optimization - skip leading zeros
-      //      }
-      //      // Preserve exactly as many leading encoded zeros in output as there
-      //      // were leading zeros in input.
-      //      while ( {
-      //        outputStart < encoded.length && encoded(outputStart) == ENCODED_ZERO
-      //      }) outputStart += 1
-      //      while ( {
-      //        {
-      //          zeros -= 1; zeros
-      //        } >= 0
-      //      }) encoded({
-      //        outputStart -= 1; outputStart
-      //      }) = ENCODED_ZERO
-      // Return encoded string (including encoded leading zeros).
+
+      while
+        inputStart < input.length
+      do
+        outputStart = outputStart - 1
+        val (index, out) = divmod(inputClone, inputStart,256,58)
+        encoded(outputStart) = ALPHABET(index)
+        if (out(inputStart) == 0) { inputStart = (inputStart + 1).toByte }
+        inputClone = out
+      end while
+
+      while
+        outputStart < encoded.length && encoded(outputStart) == ENCODED_ZERO
+      do
+        outputStart = outputStart + 1
+      end while
+
+      while
+        zeros >= 0
+      do
+        zeros = (zeros - 1).toByte
+        outputStart = outputStart - 1
+        encoded(outputStart) = ENCODED_ZERO
+      end while
+      outputStart = outputStart + 1
       new java.lang.String(encoded, outputStart, encoded.length - outputStart)
     end if
-  end decode
+  end encode
 
   private def divmod(in: Array[Byte], firstDigit: Int, base: Int, divisor: Int): (Byte, Array[Byte]) =
     var remainder = 0
     val out = mutable.ArrayBuilder.make[Byte].addAll(in.take(firstDigit))
     for
-      i <- firstDigit until number.length
+      i <- firstDigit until in.length
     do
       val digit = in(i).toInt & 0xFF
       val temp = remainder * base + digit
