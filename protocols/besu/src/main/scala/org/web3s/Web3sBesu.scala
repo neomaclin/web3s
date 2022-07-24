4c69a6f19d458b09403314de4623a07836f9be30
Neo <neomac.lin@gmail.com>
2022-07-24 01:33:13 -0400

    adding besu code

--- protocols/besu/src/main/scala/org/web3s/Web3sBesu.scala
+++ protocols/besu/src/main/scala/org/web3s/Web3sBesu.scala
@@ -1,80 +1,139 @@
 package org.web3s
 
 import cats.MonadThrow
+import org.web3s.protocol.core.Request
+import org.web3s.protocol.core.methods.response.*
 import org.web3s.services.Web3sService
 
 class Web3sBesu[F[_]: MonadThrow](using services: Web3sService[F]) extends Besu[F[_]]:
-  def minerStart: F[MinerStartResponse]
+  import io.circe._
+  import io.circe.syntax._
+  import io.circe.generic.auto._
 
-  def minerStop: F[BooleanResponse]
+  def minerStart: F[MinerStartResponse] =
+    services.fetch[String](Request(method = "miner_start")).map(MinerStartResponse.apply)
 
-  def cliqueDiscard(address: String): F[BooleanResponse]
+  def minerStop: F[BooleanResponse] =
+    services.fetch[String](Request(method = "miner_stop")).map(BooleanResponse.apply)
 
-  def cliqueGetSigners(defaultBlockParameter: DefaultBlockParameter): F[EthAccounts]
 
-  def cliqueGetSignersAtHash(blockHash: String): F[EthAccounts]
+  def cliqueDiscard(address: String): F[BooleanResponse]=
+    services.fetch[String](Request(method = "clique_discard")).map(BooleanResponse.apply)
 
-  def cliquePropose(address: String, signerAddition: Boolean): F[BooleanResponse]
+  def cliqueGetSigners(defaultBlockParameter: DefaultBlockParameter): F[EthAccounts]=
+    services.fetch[String](Request(method = "clique_getSigners")).map(EthAccounts.apply)
 
-  def cliqueProposals: F[BesuEthAccountsMapResponse]
+  def cliqueGetSignersAtHash(blockHash: String): F[EthAccounts]=
+    services.fetch[String](Request(method = "clique_getSignersAtHash")).map(EthAccounts.apply)
 
-  def debugTraceTransaction(transactionHash: String, options: Map[String, Boolean]): F[BesuFullDebugTraceResponse]
+  def cliquePropose(address: String, signerAddition: Boolean): F[BooleanResponse]=
+    services.fetch[String](Request(method = "clique_propose")).map(BooleanResponse.apply)
 
-  def ibftDiscardValidatorVote(address: String): F[BooleanResponse]
+  def cliqueProposals: F[BesuEthAccountsMapResponse]=
+    services.fetch[String](Request(method = "clique_proposals")).map(BesuEthAccountsMapResponse.apply)
 
-  def ibftGetPendingVotes: F[BesuEthAccountsMapResponse]
+  def debugTraceTransaction(transactionHash: String, options: Map[String, Boolean]): F[BesuFullDebugTraceResponse]=
+    services.fetch[String](Request(method = "debug_traceTransaction")).map(BesuFullDebugTraceResponse.apply)
 
-  def ibftGetSignerMetrics: F[BesuSignerMetrics]
+  def ibftDiscardValidatorVote(address: String): F[BooleanResponse]=
+    services.fetch[String](Request(method = "ibft_discardValidatorVote")).map(BooleanResponse.apply)
 
-  def ibftGetValidatorsByBlockNumber(defaultBlockParameter: DefaultBlockParameter): F[EthAccounts]
 
-  def ibftGetValidatorsByBlockHash(blockHash: String): F[EthAccounts]
+  def ibftGetPendingVotes: F[BesuEthAccountsMapResponse] =
+    services.fetch[String](Request(method = "ibft_getPendingVotes")).map(BesuEthAccountsMapResponse.apply)
 
-  def ibftProposeValidatorVote(address: String, validatorAddition: Boolean): F[BooleanResponse]
+  def ibftGetSignerMetrics: F[BesuSignerMetrics]=
+    services.fetch[String](Request(method = "ibft_getSignerMetrics")).map(BesuSignerMetrics.apply)
 
-  def privGetTransactionCount(address: String, privacyGroupId: Base64String): F[EthGetTransactionCount]
 
-  def privGetPrivateTransaction(transactionHash: String): F[PrivGetPrivateTransaction]
+  def ibftGetValidatorsByBlockNumber(defaultBlockParameter: DefaultBlockParameter): F[EthAccounts]=
+    services.fetch[String](Request(method = "ibft_getValidatorsByBlockNumber")).map(EthAccounts.apply)
 
-  def privDistributeRawTransaction(signedTransactionData: String): F[PrivateEnclaveKey]
 
-  def privGetPrivacyPrecompileAddress: F[PrivGetPrivacyPrecompileAddress]
+  def ibftGetValidatorsByBlockHash(blockHash: String): F[EthAccounts]=
+    services.fetch[String](Request(method = "ibft_getValidatorsByBlockHash")).map(EthAccounts.apply)
 
-  def privCreatePrivacyGroup(addresses: List[Base64String], name: String, description: String): F[PrivCreatePrivacyGroup]
 
-  @throws[IOException]
-  def privOnChainSetGroupLockState(privacyGroupId: Base64String, credentials: Credentials, enclaveKey: Base64String, lock: Boolean): F[EthSendTransaction]
+  def ibftProposeValidatorVote(address: String, validatorAddition: Boolean): F[BooleanResponse]=
+    services.fetch[String](Request(method = "ibft_proposeValidatorVote")).map(BooleanResponse.apply)
 
-  @throws[IOException]
-  @throws[TransactionException]
-  def privOnChainAddToPrivacyGroup(privacyGroupId: Base64String, credentials: Credentials, enclaveKey: Base64String, participants: List[Base64String]): F[EthSendTransaction]
 
-  @throws[IOException]
-  def privOnChainCreatePrivacyGroup(privacyGroupId: Base64String, credentials: Credentials, enclaveKey: Base64String, participants: List[Base64String]): F[EthSendTransaction]
+  def privGetTransactionCount(address: String, privacyGroupId: Base64String): F[EthGetTransactionCount] =
+    services.fetch[String](Request(method = "priv_getTransactionCount")).map(EthGetTransactionCount.apply)
 
-  @throws[IOException]
-  def privOnChainRemoveFromPrivacyGroup(privacyGroupId: Base64String, credentials: Credentials, enclaveKey: Base64String, participant: Base64String): F[EthSendTransaction]
+  def privGetPrivateTransaction(transactionHash: String): F[PrivGetPrivateTransaction] =
+    services.fetch[String](Request(method = "priv_getPrivateTransaction")).map(PrivGetPrivateTransaction.apply)
 
-  def privOnChainFindPrivacyGroup(addresses: List[Base64String]): F[PrivFindPrivacyGroup]
 
-  def privFindPrivacyGroup(addresses: List[Base64String]): F[PrivFindPrivacyGroup]
+  def privDistributeRawTransaction(signedTransactionData: String): F[PrivateEnclaveKey]=
+    services.fetch[String](Request(method = "priv_distributeRawTransaction")).map(PrivateEnclaveKey.apply)
 
-  def privDeletePrivacyGroup(privacyGroupId: Base64String): F[BooleanResponse]
 
-  def privGetTransactionReceipt(transactionHash: String): F[PrivGetTransactionReceipt]
+  def privGetPrivacyPrecompileAddress: F[PrivGetPrivacyPrecompileAddress]=
+    services.fetch[String](Request(method = "priv_getPrivacyPrecompileAddress")).map(PrivGetPrivacyPrecompileAddress.apply)
 
-  def privGetCode(privacyGroupId: String, address: String, defaultBlockParameter: DefaultBlockParameter): F[EthGetCode]
 
-  def privCall(privacyGroupId: String, transaction: Transaction, defaultBlockParameter: DefaultBlockParameter): F[EthCall]
+  def privCreatePrivacyGroup(addresses: List[Base64String], name: String, description: String): F[PrivCreatePrivacyGroup]=
+    services.fetch[String](Request(method = "priv_createPrivacyGroup")).map(PrivCreatePrivacyGroup.apply)
 
-  def privGetLogs(privacyGroupId: String, ethFilter: EthFilter): F[EthLog]
 
-  def privNewFilter(privacyGroupId: String, ethFilter: EthFilter): F[EthFilter]
+//  def privOnChainSetGroupLockState(privacyGroupId: Base64String, credentials: Credentials, enclaveKey: Base64String, lock: Boolean): F[EthSendTransaction]=
+//    services.fetch[String](Request(method = "web3_clientVersion")).map(Web3ClientVersion.apply)
+//
+//  def privOnChainAddToPrivacyGroup(privacyGroupId: Base64String, credentials: Credentials, enclaveKey: Base64String, participants: List[Base64String]): F[EthSendTransaction]=
+//    services.fetch[String](Request(method = "web3_clientVersion")).map(Web3ClientVersion.apply)
+//
+//
+//  def privOnChainCreatePrivacyGroup(privacyGroupId: Base64String, credentials: Credentials, enclaveKey: Base64String, participants: List[Base64String]): F[EthSendTransaction]=
+//    services.fetch[String](Request(method = "web3_clientVersion")).map(Web3ClientVersion.apply)
+//
+//
+//  def privOnChainRemoveFromPrivacyGroup(privacyGroupId: Base64String, credentials: Credentials, enclaveKey: Base64String, participant: Base64String): F[EthSendTransaction]=
+//    services.fetch[String](Request(method = "web3_clientVersion")).map(Web3ClientVersion.apply)
+//
 
-  def privUninstallFilter(privacyGroupId: String, filterId: String): F[EthUninstallFilter]
+  def privOnChainFindPrivacyGroup(addresses: List[Base64String]): F[PrivFindPrivacyGroup]=
+    services.fetch[String](Request(method = "privx_findOnChainPrivacyGroup")).map(PrivFindPrivacyGroup.apply)
 
-  def privGetFilterChanges(privacyGroupId: String, filterId: String): F[EthLog]
 
-  def privGetFilterLogs(privacyGroupId: String, filterId: String): F[EthLog]
+  def privFindPrivacyGroup(addresses: List[Base64String]): F[PrivFindPrivacyGroup]=
+    services.fetch[String](Request(method = "priv_findPrivacyGroup")).map(PrivFindPrivacyGroup.apply)
+
+
+  def privDeletePrivacyGroup(privacyGroupId: Base64String): F[BooleanResponse]=
+    services.fetch[String](Request(method = "priv_deletePrivacyGroup")).map(BooleanResponse.apply)
+
+
+  def privGetTransactionReceipt(transactionHash: String): F[PrivGetTransactionReceipt]=
+    services.fetch[String](Request(method = "priv_getTransactionReceipt")).map(PrivGetTransactionReceipt.apply)
+
+
+  def privGetCode(privacyGroupId: String, address: String, defaultBlockParameter: DefaultBlockParameter): F[EthGetCode]=
+    services.fetch[String](Request(method = "priv_getCode")).map(EthGetCode.apply)
+
+
+  def privCall(privacyGroupId: String, transaction: Transaction, defaultBlockParameter: DefaultBlockParameter): F[EthCall]=
+    services.fetch[String](Request(method = "priv_call")).map(EthCall.apply)
+
+
+  def privGetLogs(privacyGroupId: String, ethFilter: EthFilter): F[EthLog]=
+    services.fetch[String](Request(method = "priv_getLogs")).map(EthLog.apply)
+
+
+  def privNewFilter(privacyGroupId: String, ethFilter: EthFilter): F[EthFilter]=
+    services.fetch[String](Request(method = "priv_newFilter")).map(EthFilter.apply)
+
+
+  def privUninstallFilter(privacyGroupId: String, filterId: String): F[EthUninstallFilter]=
+    services.fetch[String](Request(method = "priv_uninstallFilter")).map(EthUninstallFilter.apply)
+
+
+  def privGetFilterChanges(privacyGroupId: String, filterId: String): F[EthLog]=
+    services.fetch[String](Request(method = "priv_getFilterChanges")).map(EthLog.apply)
+
+
+  def privGetFilterLogs(privacyGroupId: String, filterId: String): F[EthLog] =
+    services.fetch[String](Request(method = "priv_getFilterLogs")).map(EthLog.apply)
+
 
 
